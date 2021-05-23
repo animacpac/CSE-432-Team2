aef91ade82b2245727661d3765709070a2b47b3c
Nathan Spotten <spottenn@byui.edu>
2021-05-22 20:34:00 -0600

    `runtime error fixes`

--- W05LabPathHomograph/W05LabPathHomograph.cpp
+++ W05LabPathHomograph/W05LabPathHomograph.cpp
@@ -20,6 +20,7 @@ const int TEST_SIZE = 10;
 const int FORBIDDEN_FILE_SIZE = 1;
 
 
+const string TEST_DIRECTORY = "C:\\Users\\user\\secret";
 
 const string TEST_FORBIDDEN_FILES[FORBIDDEN_FILE_SIZE]{
     // Test for a forbidden.
@@ -65,9 +66,9 @@ const string PATH_SYMBOLS[] = {
 
 
 void runTests();
-string canonicalize(string path); // Nathan
+string canonicalize(string path, string currentDirectory); // Nathan
 bool prompt();
-bool isHomograph(string path1, string path2);
+bool isHomograph(string path1, string path2, string currentDirectory);
 
 
 void runTests()
@@ -79,7 +80,7 @@ void runTests()
     //Loop and tests  all homograph examples
     for (int h = 0; h < TEST_SIZE; h++) {
         for (int f = 0; f < FORBIDDEN_FILE_SIZE; f++) {
-            if (isHomograph(TEST_HOMOGRAPHS[h], TEST_FORBIDDEN_FILES[f]) == false) {
+            if (isHomograph(TEST_HOMOGRAPHS[h], TEST_FORBIDDEN_FILES[f],TEST_DIRECTORY) == false) {
 
 
                 cout << "Failed Homograph Examples at test line: "
@@ -92,7 +93,7 @@ void runTests()
     //Loop and tests all Non-Homograph examples
     for (int n = 0; n < TEST_SIZE; n++) {
         for (int f = 0; f < FORBIDDEN_FILE_SIZE; f++) {
-            if (isHomograph(TEST_NON_HOMOGRAPHS[n], TEST_FORBIDDEN_FILES[f])) {
+            if (isHomograph(TEST_NON_HOMOGRAPHS[n], TEST_FORBIDDEN_FILES[f], TEST_DIRECTORY) ){
 
                 cout << "Failed Non-Homograph Examples at test line: "
                     << n + 1 << " at the Forbidden line: " << f + 1 << endl;
@@ -104,7 +105,7 @@ void runTests()
     if (passedAllHomographTest && passedAllNonHomographTest)
     {
 
-        cout << "Success in all tests";
+        cout << "Success in all tests" << endl;
     }
     return;
 }
@@ -112,7 +113,7 @@ void runTests()
  * GetCurrentPath
  * The function which gets current Path
  ***********************************************************************/
-std::string get_current_dir() {
+string get_current_dir() {
 	char buff[FILENAME_MAX]; //create string buffer to hold path
 	GetCurrentDir(buff, FILENAME_MAX);
 	string current_working_dir(buff);
@@ -120,7 +121,7 @@ std::string get_current_dir() {
 }
 
 
-string canonicalize(string path)
+string canonicalize(string path, string currentDirectory)
 {
 
 
@@ -138,8 +139,6 @@ string canonicalize(string path)
    replace(path.begin(), path.end(), '/', '\\');
 
    // a. get the current directory from windows
-    string currentDirectory = get_current_dir();
-   currentDirectory =     "C:\\Users\\user\\secret";
    for (int i = 0; i < currentDirectory.length(); i++) {
       currentDirectory[i] = tolower(currentDirectory[i]);
    }
@@ -254,11 +253,11 @@ bool prompTest()
 	}
 }
 
-bool isHomograph(string path1, string path2)
+bool isHomograph(string path1, string path2, string currentDirectory)
 {
 // This is just an example on how it should be done
-   string canoncalizedFile1 = canonicalize(path1);
-   string canoncalizedFile2 = canonicalize(path2);
+   string canoncalizedFile1 = canonicalize(path1, currentDirectory);
+   string canoncalizedFile2 = canonicalize(path2, currentDirectory);
    if (canoncalizedFile1 == canoncalizedFile2) {
       cout << "The paths are homographs.\n";
       return true;
@@ -287,7 +286,7 @@ int main(int argc, char* argv[]) {
     cout << "Specify the second filename: ";
     cin >> file2;
 
-    isHomograph(file1, file2);
+   isHomograph(file1, file2, get_current_dir());
 
 	return 0;
 }
