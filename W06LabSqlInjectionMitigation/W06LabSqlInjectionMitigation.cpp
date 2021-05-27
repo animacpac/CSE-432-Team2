14bc7155b7adc7af4c7fcb1300e45d46e36f2dad
animacpac <56101332+animacpac@users.noreply.github.com>
2021-05-26 19:44:16 -0600

    test

--- W06LabSqlInjectionMitigation/W06LabSqlInjectionMitigation.cpp
+++ W06LabSqlInjectionMitigation/W06LabSqlInjectionMitigation.cpp
@@ -36,7 +36,10 @@ const string TESTS_TAUTOLOGY[TESTS_SIZE][TEST_PARAMETERS_SIZE] = {
 
 const string TESTS_UNION[TESTS_SIZE][TEST_PARAMETERS_SIZE] = {
         {"username1", "password1"}, // Nathan
-        {"vbarret", "password1"}, // Valter
+        {"vbarret", "1\'; DELETE FROM users WHERE 1=1","nothing\'; INSERT INTO users (name, passwd) VALUES \'Awesome\', \'1234",
+                                "WHERE id = \'username\' UNION SELECT Username, Password FROM users;",
+                                "\'; SELECT * FROM users WHERE user_id = Username; DROP TABLE users",
+                                "\'; SELECT * FROM users WHERE id = 0 OR 1=1"}, // Valter
         {"username1", "password1"}, // Phillip
         {"username1", "password1"}, // Mark
         {"username2", "password2"} // Everton
